c741cd7fa817ff2da180dd5c2b42897fae266d15
mballo <marcoballotti1998@gmail.com>
2021-11-30 23:04:40 +0100

    add call to open door and close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private http: HttpClient) { }
+
+  status: string = "APRI";
   title = 'angular-apriporta';
+  active: boolean = true;
+
+  ngOnInit(){
+
+  }
+
+  openDoor(){
+    this.active = false;
+    this.http.get('http://192.168.1.250/leds.cgi?led=0').subscribe(res => {
+      console.log("OPEN");
+      setTimeout(()=>{                           // <<<---using ()=> syntax
+        this.http.get('http://192.168.1.250/leds.cgi?led=0').subscribe(res => {
+        console.log("CLOSE");
+        this.active = true;
+        })
+    }, 3000);
+
+
+    })
+  }
 }
